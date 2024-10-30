e11ebc930bbe676c798815a41aa74eb6da659c31
douglas-resende <douglasmresende@gmail.com>
2024-10-30 17:07:18 -0300

    :construction: feat: implementando swagger

--- src/server.ts
+++ src/server.ts
@@ -8,11 +8,30 @@ import configurePassport from "./config/passport";
 import connectDB from "./config/connectionDB";
 import passport from "passport";
 
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
+
+// Swagger
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Chat Backend",
+      version: "1.0.0",
+      description: "API para autenticação e chat em tempo real",
+    },
+  },
+  // TODO - ajustar depois separando em arquivos
+  apis: ["./routes/*.ts"],
+};
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 configurePassport();
 app.use(passport.initialize());
